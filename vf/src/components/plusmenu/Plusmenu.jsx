93256c63dda27f29af9250bdb30dc6020810ccbd
iwahju <iwahju@gmail.com>
2022-10-16 15:32:12 -0400

    vf mui brief update

--- vf/src/components/plusmenu/Plusmenu.jsx
+++ vf/src/components/plusmenu/Plusmenu.jsx
@@ -1,45 +1,84 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
 import "./plusmenu.css";
-
+import {fridgeContext} from "../../context"
 
 function Plusmenu() {
+  const defaultformstate = {
+    name: "",
+    quantity: 0,
+    "exp-date": "",
+    location: "fridge",
+  };
+  const [formstate, setFormstate] = useState(defaultformstate);
+  const context = useContext(fridgeContext)
+  console.log(context)
+  const handleInputChange = (e) => {
+    setFormstate({
+      ...formstate,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //location = fridge, pantry
+  const handleFridgeClick = (location,e) => {
+    e.preventDefault()
+    setFormstate({
+      ...formstate,
+      location,
+    });
+    console.log(formstate);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(formstate);
+  };
+
   return (
-    <div className='plusmenu-outer-container'>
-        <div className='plusmenu-inner-container'>
-            <div>
-                <h3>Add Item</h3>
-            </div>
-            <div>
-                <form className='form'>
-                    <div className='form-title'>
-                    <input 
-                        type='text'
-                        placeholder='Item Name'
-                    />
-                    </div>
-                    <div className='form-quan-date'>
-                    
-                    <input
-                        type='text'
-                        placeholder='quantity'
-                    />
-                    <input
-                    type='text'
-                    placeholder='Exp. Date'
-                    />
-
-                    </div>
-                    <div>
-                        <button>Fridge</button>
-                        <button>Pantry</button>
-                    </div>
-                    <button type='submit'>Add</button>
-                </form>
-            </div>
+    <div className="plusmenu-outer-container">
+      <div className="plusmenu-inner-container">
+        <div>
+          <h3>Add Item</h3>
         </div>
-
+        <div>
+          <form className="form" onSubmit={handleFormSubmit}>
+            <div className="form-title">
+              <input
+                name="name"
+                value={formstate.name}
+                type="text"
+                placeholder="Item Name"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-quan-date">
+              <input
+                name="quantity"
+                value={formstate.quantity}
+                type="text"
+                placeholder="quantity"
+                onChange={handleInputChange}
+              />
+              <input
+                name="exp-date"
+                value={formstate["exp-date"]}
+                type="text"
+                placeholder="Exp. Date"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <button onClick={(e) => handleFridgeClick("fridge",e)}>
+                Fridge
+              </button>
+              <button onClick={(e) => handleFridgeClick("pantry",e)}>
+                Pantry
+              </button>
+            </div>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Plusmenu
+export default Plusmenu;
