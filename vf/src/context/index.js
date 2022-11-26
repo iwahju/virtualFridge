51c90fc21852220edea1d1695b1d7c2f9cb6868d
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2022-11-26 14:43:41 -0500

    update the VF folder to frontend folder

--- vf/src/context/index.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const fridgeContext = createContext(null);
-const FridgeProvider = fridgeContext.Provider;
-
-export { fridgeContext, FridgeProvider };
--- vf/src/demo/demo.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-
-function Demo() {
-  return <Button variant="contained">Hello World</Button>;
-}
-
-export default Demo;
