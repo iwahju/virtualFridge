e93a0585ec8e6f092e4a24642e04713d8e8b2700
LouisMullarkey <louis.mullarkey@emory.edu>
2022-12-04 19:51:28 -0500

    Grocery List Work

--- frontend/src/components/listitemmenu/ListItemMenu.js
+++ frontend/src/components/listitemmenu/ListItemMenu.js
@@ -1,7 +1,22 @@
-import React from 'react';
 import "./listitemmenu.css";
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import React, { useState } from "react";
 
 function ListItemMenu() {
+    const defaultformstate = {
+        ingredient: "",
+        quantity: 0,
+        unit: "",
+        fridge: "true"
+    };
+    const [formstate, setFormstate] = useState(defaultformstate);
+    const handleInputChange = (e) => {
+        setFormstate({
+          ...formstate,
+          [e.target.name]: e.target.value,
+        });
+      };
   return (
     <div className='listitemmenu-outer-container'>
         <div className='listitemmenu-inner-container'>
@@ -19,6 +34,11 @@ function ListItemMenu() {
                     <input 
                         type='text'
                         placeholder='Item'
+
+                      name= "ingredient"
+
+                      value={formstate.ingredient}
+                      onChange={handleInputChange}
                     />
                     </div>
                 </div>
@@ -31,12 +51,46 @@ function ListItemMenu() {
                     <input
                         type='text'
                         placeholder='Quantity'
+
+                        name= "quantity"
+                        value={formstate.quantity}
+                        onChange={handleInputChange}
                     />
                     </div>
                 </div>
-                    <button className="submit-button" type='submit'>Add</button>
-                    
+
+                <div className='unit-container'>
+                <div className='text-container'>
+                        Unit
+                    </div>
+                <Select
+                      
+                      labelId="unit-label"
+                      id="demo-simple-select-standard"
+                      value={formstate.unit}
+                      name= "unit"
+                      onChange={handleInputChange}
+                      label="unit"
+                    >
+                      <MenuItem value="">
+                      </MenuItem>
+                      <input type= "text" 
+                      placeholder="other"
+                      name= "unit"
+                      />
+                      <MenuItem value={'lb'}>lbs</MenuItem>
+                      <MenuItem value={'tsp'}>tsp</MenuItem>
+                      <MenuItem value={'tbsp'}>tspb</MenuItem>
+                      <MenuItem value={'gallon'}>gallon</MenuItem>
+                      <MenuItem value={'ounce'}>ounce(s)</MenuItem>
+                    </Select>
+
+
+                </div>
+                
+                <button className="submit-button" type='submit'>Add</button>
                 </form>
+
             </div>
             
         </div>
