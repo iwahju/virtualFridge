a0c07ab51a9d489ef8a03757be80b8b9b301b81a
LouisMullarkey <louis.mullarkey@emory.edu>
2022-12-06 19:43:33 -0500

    Display

--- frontend/src/components/pages/MyRecipe.js
+++ frontend/src/components/pages/MyRecipe.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./myrecipe.css";
 import RecipeButton from "../recipebutton/RecipeButton";
+import axios from "axios";
+import { List, ListItem, ListItemText, Stack } from "@mui/material";
 
 
 function MyRecipe(props) {
+  
+  const [allRecipes, setAllRecipes] = useState([]);
+  const [isRecipesLoaded, setRecipesLoaded] = useState(false);
+
+  useEffect(() => {
+    // get recipes from API
+    if (isRecipesLoaded === false) {
+      axios({
+        method: "GET",
+        url: "/userRecipes",
+        headers: {
+          Authorization: `Bearer  ${props.token}`,
+        },
+      })
+        .then((response) => {
+          setAllRecipes(response.data);
+        })
+        .then(() => {
+          setRecipesLoaded(true);
+        });
+    }
+  });
+
+  const inventoryItem = (item, index) => (
+    <ListItem key={item.name + "__" + index}>
+      <ListItemText>{item.name}</ListItemText>
+    </ListItem>
+  );
+
   return (
     <div className = "myrecipe">
     <div className = "appAside1">
@@ -21,11 +52,13 @@ function MyRecipe(props) {
       }}
       />
       <div className="storagebox1">
+      <Stack className="storagebox-content1" color={'white'}>
+            <List>{allRecipes.map(inventoryItem)}</List>
+      </Stack>
       </div>
     </div>
     <div className = "appAside2">
     <div className="storagebox-title2">
-          BOOKMARKED RECIPES
         </div>
         <hr 
       style={{
